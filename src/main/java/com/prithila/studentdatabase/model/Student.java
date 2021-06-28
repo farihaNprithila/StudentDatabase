d6fbdde64ca2dd0dfaa316dce17be16edac1f82c
FarihaNawaz <48926061+FarihaNawaz@users.noreply.github.com>
2021-06-29 01:47:42 +0600

    Adding equals and hashcode in Student POJO

--- src/main/java/com/prithila/studentdatabase/model/Student.java
+++ src/main/java/com/prithila/studentdatabase/model/Student.java
@@ -2,6 +2,7 @@ package com.prithila.studentdatabase.model;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author Prithila
@@ -100,4 +101,16 @@ public class Student {
         this.courseList = courseList;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentID.equals(student.studentID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentID);
+    }
 }
